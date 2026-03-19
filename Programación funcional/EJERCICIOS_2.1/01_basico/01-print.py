a62d6dc9f8d8e89ace9ed3f398c4d1f0d76db65c
VictorE14 <tuzdzidzv@gmail.com>
2026-03-18 20:02:05 -0500

    Add files via upload

---
+++ Programación funcional/EJERCICIOS_2.1/01_basico/01-print.py
@@ -0,0 +1,24 @@
+from os import system
+
+if system ("clear") !=0: system ("cls")
+
+print('Hola, Crayola!')
+print ('Esto tambien funciona con una comilla')
+print ('Python', 'es', 'genial')
+
+print("Python", 'es', 'brutal', sep= "-")
+
+print ('Esto se imprime', end='\n')
+
+print('en una linea')
+
+
+
+#IMPRIMIR NUMEROS 
+
+print(43)
+
+print('Esto es una "pULGADA" dentro de un string con comillas simples')
+print( "esto \"pulgada\" string")
+
+print("""esto "pulgada" igual """)
